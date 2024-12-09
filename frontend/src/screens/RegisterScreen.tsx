f8513bdb868aa0dc5ba79108193c2f914b212744
Bruno Falckete <bruno_fonseca_3l@hotmail.com>
2024-12-08 21:55:33 -0400

    Fix alert

--- frontend/src/screens/RegisterScreen.tsx
+++ frontend/src/screens/RegisterScreen.tsx
@@ -100,7 +100,6 @@ const RegisterScreen: React.FC<Props> = ({ navigation }) => {
       // Verifica se userContext e setLoggedInUser existem
       addUser(newUser);
       userContext.setLoggedInUser(newUser); // Define o usuário como logado
-      setAlertMessage("Usuário registrado com sucesso!");
       setAlertVisible(true);
       navigation.navigate("ImageUpload");
     } else {
